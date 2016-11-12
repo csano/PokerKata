ccecba6800e35c3c400841f587d996c5beeeffeb
Chris Sano <cmsano@gmail.com>
2016-11-12 12:55:40 -0800

    Added additional unit tests around different card permutations

--- PokerKata.UnitTests/AddingCardsToHand.cs
+++ PokerKata.UnitTests/AddingCardsToHand.cs
@@ -41,5 +41,39 @@ namespace PokerKata.UnitTests
 
             hand.ToString().ShouldBeEquivalentTo("Ad, Kd");
         }
+
+        [Fact]
+        public void AddingMixedCardsResultsInHandBeingInExpectedOrder()
+        {
+            var card1 = new Card(new King(), new Diamond());
+            var card2 = new Card(new Ace(), new Diamond());
+            var card3 = new Card(new Ace(), new Club());
+            var card4 = new Card(new Two(), new Heart());
+            var hand = new Hand();
+
+            hand.Add(card1);
+            hand.Add(card2);
+            hand.Add(card3);
+            hand.Add(card4);
+
+            hand.ToString().ShouldBeEquivalentTo("Ac, Ad, Kd, 2h");
+        }
+
+        [Fact]
+        public void AddingAllFourAcesResultsInAcesBeingListedInOrder()
+        {
+            var card1 = new Card(new Ace(), new Spade());
+            var card2 = new Card(new Ace(), new Diamond());
+            var card3 = new Card(new Ace(), new Club());
+            var card4 = new Card(new Ace(), new Heart());
+            var hand = new Hand();
+
+            hand.Add(card1);
+            hand.Add(card2);
+            hand.Add(card3);
+            hand.Add(card4);
+
+            hand.ToString().ShouldBeEquivalentTo("Ac, Ad, Ah, As");
+        }
     }
 }
