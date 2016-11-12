38b6b49629f0fa97b337bb721716889d918ab407
Chris Sano <cmsano@gmail.com>
2016-11-12 10:45:07 -0800

    Updating file after save

--- PokerKata.UnitTests/Cards/Values/Comparing/ComparingValue.cs
+++ PokerKata.UnitTests/Cards/Values/Comparing/ComparingValue.cs
@@ -1,31 +1,9 @@
 ﻿using FluentAssertions;
 using PokerKata.Cards;
-using PokerKata.Cards.Values;
 using Xunit;
 
 namespace PokerKata.UnitTests.Cards.Values.Comparing
 {
-    public class TestValue : Value
-    {
-        public override int Rank => 100;
-
-        public override string ToString()
-        {
-            return "ZZZZ";
-        }
-    }
-
-    public class ComparingJackValue : ComparingValue<JackValue> { }
-    public class ComparingTenValue : ComparingValue<TenValue> { }
-    public class ComparingNineValue : ComparingValue<NineValue> { }
-    public class ComparingEightValue : ComparingValue<EightValue> { }
-    public class ComparingSevenValue : ComparingValue<SevenValue> { }
-    public class ComparingSixValue : ComparingValue<SixValue> { }
-    public class ComparingFiveValue : ComparingValue<FiveValue> { }
-    public class ComparingFourValue : ComparingValue<FourValue> { }
-    public class ComparingThreeValue : ComparingValue<ThreeValue> { }
-    public class ComparingTwoValue : ComparingValue<TwoValue> { }
-
     public abstract class ComparingValue<T> where T: Value, new()
     {
         [Fact]
